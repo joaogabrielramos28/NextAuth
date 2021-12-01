d9d5cf9da674b5dc335d52b908663e252bce6181
joaogabrielramos28 <joaogabrielramos28@outlook.com>
2021-12-01 19:54:02 -0300

    Project initial adjustments

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Next Auth App</title>
+      </Head>
+
+        <h2>Auth Application</h2>
+    </>
+  )
+}
+
+export default Home
